0e6290097687707291a9b3754120175ed5fd4688
rootz491 <experimentyt1@gmail.com>
2022-01-28 17:32:55 +0530

    changes for pi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const { Client, Intents, Collection } = require('discord.js');
+const http = require('http');
 require('dotenv').config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_PRESENCES] });
